ec1bba5442d7ba10c8661cb5371b535129d4aa64
JohnnatanV <joh.villaneda@outlook.com>
2021-06-02 21:51:34 -0500

    Updating storage info in app

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const amountInfo = document.querySelector(".amount-info");
 const reasonInfo = document.querySelector(".reason-info");
 //Submit
 const expenseBtn = document.querySelector(".expense-button");
+const clearAll = document.querySelector(".clear");
 // List
 const expenseList = document.querySelector(".expense-list");
 const del = document.querySelectorAll(".delete");
@@ -18,6 +19,7 @@ const list = document.querySelector(".list");
 document.addEventListener("DOMContentLoaded", getExpenses);
 walletBtn.addEventListener("click", addWallet);
 expenseBtn.addEventListener("click", addExpense);
+clearAll.addEventListener("click", clean);
 list.addEventListener("click", deleteTrash);
 
 // Functions
@@ -25,23 +27,12 @@ function addWallet() {
   const data = walletInput.value;
   localStorage.setItem("wallet", data);
   hideShow();
-  wallet.innerText = `$ ${data}`;
 }
 
-// HIDE AND SEEK
-function hideShow() {
-  if (localStorage.getItem("wallet") !== null) {
-    const hide = document.getElementById("display-wallet");
-    hide.style.display = "none";
-
-    const show = document.getElementById("display-form");
-    show.style.display = "flex";
-  }
-}
 // ADD EXPENSES
 function addExpense(event) {
   //Prevent Submiting Reaload
-  event.preventDefault();
+  // event.preventDefault();
   // Expense TR
   const expenseDiv = document.createElement("tr");
   expenseDiv.classList.add("expense");
@@ -57,7 +48,7 @@ function addExpense(event) {
 
   const btnDelete = document.createElement("td");
   btnDelete.classList.add("btn-delete");
-  btnDelete.innerHTML = '<input type="button" class="delete" value="X" />';
+  btnDelete.innerHTML = '<button class="delete">X</button>';
   expenseDiv.appendChild(btnDelete);
 
   //ADD TO LOCAL STORAGE
@@ -115,7 +106,7 @@ function getExpenses() {
 
     const btnDelete = document.createElement("td");
     btnDelete.classList.add("btn-delete");
-    btnDelete.innerHTML = '<input type="button" class="delete" value="X" />';
+    btnDelete.innerHTML = '<button class="delete">X</button>';
     expenseDiv.appendChild(btnDelete);
 
     list.appendChild(expenseDiv);
@@ -124,20 +115,20 @@ function getExpenses() {
 
 //DELETE EXPENCE
 function deleteTrash(e) {
-  removeLocal();
-  const item = e.target;
-  let tr = item.closest("tr");
-  // tr.remove();
-  //delete
-}
-
-function removeLocal() {
   catchExpense();
 
-  expenses.forEach((x) => {
-    let td = document.querySelectorAll("td");
+  const item = e.target;
+  let tr = item.closest("tr");
+  let node1 = tr.childNodes[0].innerText.split(" ").splice(1, 1)[0];
+  let node2 = tr.childNodes[1].innerText;
 
-    console.log(td);
+  expenses.forEach((item) => {
+    if (item[0] === node1 && item[1] === node2) {
+      let dataIndex = expenses.indexOf(item);
+      expenses.splice(dataIndex, 1);
+      localStorage.setItem("expenses", JSON.stringify(expenses));
+      location.reload();
+    }
   });
 }
 
@@ -149,6 +140,24 @@ function show() {
   pocket.innerText = `$ -${total}`;
 
   let insideWallet = parseFloat(localStorage.getItem("wallet"));
+
   insideWallet = insideWallet - total;
   wallet.innerText = `$ ${insideWallet}`;
+}
+
+// HIDE AND SEEK
+function hideShow() {
+  if (localStorage.getItem("wallet") !== null) {
+    const hide = document.getElementById("display-wallet");
+    hide.style.display = "none";
+    wallet.innerText = `$ ${localStorage.getItem("wallet")}`;
+
+    const show = document.getElementById("display-form");
+    show.style.display = "flex";
+  }
+}
+
+function clean() {
+  localStorage.clear();
+  location.reload();
 }
